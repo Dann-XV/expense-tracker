24d64d42ca67a9f099767094fa3a69cb9dc321ec
Daniel Bamgbose <dharnielb15@gmail.com>
2023-11-21 09:51:10 +0100

    initial files

---
+++ index.js
@@ -0,0 +1,12 @@
+let balance = document.querySelector('.balance');
+let income = document.querySelector('.income-value');
+let expense = document.querySelector('.expense-value');
+const description = document.querySelector('#description');
+const amount = document.querySelector('#amount');
+const submitBtn = document.querySelector('.submit-btn');
+
+
+submitBtn.addEventListener('click', () => {
+    // console.log('clicked')
+    // event.preventDefault()
+})
